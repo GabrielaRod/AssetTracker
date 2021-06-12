77b348de0b0f875fe367b225725d70ed03c6bc24
Gabriela <GabrielaRod@users.noreply.github.com>
2021-06-12 00:43:43 -0400

    On the works
    TODO: Finish getData function to populate Spinner and get the vehicle data.

--- app/src/main/java/com/example/assettracker/Alert.java
+++ app/src/main/java/com/example/assettracker/Alert.java
@@ -1,24 +1,39 @@
 package com.example.assettracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.Toast;
 
-import com.vishnusivadas.advanced_httpurlconnection.FetchData;
-import com.vishnusivadas.advanced_httpurlconnection.PutData;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Alert extends AppCompatActivity {
 
     Button buttonSubmit, buttonCancel;
-    Spinner tags;
+    Spinner tagSpinner;
+    ArrayList<String> tagList;
+    ArrayAdapter<String> tagidAdapter;
+    RequestQueue requestQueue;
+    String URL, sessionemail, tagChoice;
+    SessionManager sessionManager;
 
 
     @Override
@@ -26,53 +41,122 @@ public class Alert extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alert);
 
-        /*Spinner (Drop Down Menu's)*/
-        tags = findViewById(R.id.tags_spinner);
+        /*Initialize Spinner (Drop Down Menu's)*/
+        tagSpinner = findViewById(R.id.tags_spinner);
 
-        /*On click listener for the Cancel button in case the user wants to cancel registration*/
-        buttonCancel.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
-                startActivity(intent);
-                finish();
-                }
-            }
+        /*Initialize the Request Queue*/
+        requestQueue = Volley.newRequestQueue(this);
 
-            /*On click listener for the submit button*/
-            buttonSubmit.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
+        /*Initializing SessionManager and Get Email*/
+        sessionManager = new SessionManager(getApplicationContext());
+        sessionemail = sessionManager.getEmail();
 
-                    /*Getting the data from the input variables*/
+        /*URL Where I get the tags from*/
+        URL = "http://10.0.0.14/LoginRegister/gettag.php";
 
 
-                    /*Error handling, if the fields are empty it will show Toast*/
-                    if(!cartype.equals("") && !tagid.equals("")){
+        /*Initialize the Arraylist*/
+        tagList = new ArrayList<>();
 
-                        Handler handler = new Handler(Looper.getMainLooper());
-                        handler.post(new Runnable() {
-                            @Override
-                            public void run() {
-                                Handler handler = new Handler(Looper.getMainLooper());
-                                handler.post(new Runnable() {
-                                    @Override
-                                    public void run() {
-                                        FetchData fetchData = new FetchData("http://10.0.0.14/LoginRegister/gettag.php");
-                                        if (fetchData.startFetch()) {
-                                            if (fetchData.onComplete()) {
-                                                String result = fetchData.getResult();
-                                                Log.i("FetchData", result);
-                                            }
-                                        }
-                                    }
-                                });
-                    }
-                    else{
-                        Toast.makeText(getApplicationContext(), "All fields are required", Toast.LENGTH_SHORT).show();
-                    }
-                }
+
+        /*Method to fetch Data from the URL*/
+        getData();
+
+        /*Initialize buttons*/
+        buttonSubmit = findViewById(R.id.submit);
+        buttonCancel = findViewById(R.id.cancel);
+
+
+        /*On click listener for the Cancel button in case the user wants to cancel and go back the main activity*/
+        buttonCancel.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            startActivity(intent);
+            finish();
+        });
+
+        /*On click listener for the submit button*/
+        buttonSubmit.setOnClickListener(v -> {
+            Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
+            startActivity(intent);
+            finish();
 
         });
     }
+
+    /* GETS THE DATA FOR THE SPINNER */
+    public void getData(){
+        /*To add parameters & values for the request*/
+        Map<String, String> params = new HashMap<>();
+        params.put("Email", sessionemail);
+
+        JSONObject parameters = new JSONObject(params);
+
+        /*Create ArrayList to store vehicle_id to later get vehicle data from DB Query*/
+        ArrayList<String> vehicleid = new ArrayList<>();
+
+        /*Initialize Json Object Request*/
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.POST, URL, parameters, response -> {
+            try {
+                //Parsing the fetched Json String to JSON Object
+                JSONArray jsonArray = response.getJSONArray("tags");
+                Log.d(String.valueOf(jsonArray), "JSON DATA");
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+                    String tag = jsonObject.optString("Tag");
+                    String vehicle = jsonObject.optString("Vehicle_Id");
+                    tagList.add(tag);
+                    vehicleid.add(vehicle);
+                    tagidAdapter = new ArrayAdapter<>(Alert.this, android.R.layout.simple_spinner_item, tagList);
+                    tagidAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                    tagSpinner.setAdapter(tagidAdapter);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                }
+            }, Throwable::getStackTrace);
+
+        /*Adding request to the queue*/
+        requestQueue.add(jsonObjectRequest);
+        tagSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                tagChoice = parent.getItemAtPosition(position).toString();
+                String idvehicle = "";
+
+                if(tagList.contains(tagChoice)){
+                    int choice = tagList.indexOf(tagChoice);
+                    idvehicle = vehicleid.get(choice);
+                }
+                else {
+                    Toast.makeText(getApplicationContext(), "The tag does not exist", Toast.LENGTH_SHORT).show();
+                }
+
+                String url = "http://10.0.0.14/LoginRegister/getvehicleinfo.php?VehicleId=" + idvehicle;
+
+                /*Initialize Json Object Request*/
+                /*TODO: Finish the request to bring the vehicle information*/
+                JsonObjectRequest jsonObjectRequest2 = new JsonObjectRequest(Request.Method.POST, url, null, response -> {
+                    try {
+                        //Parsing the fetched Json String to JSON Object
+                        JSONArray jsonArray = response.getJSONArray("tags");
+                        Log.d(String.valueOf(jsonArray), "JSON DATA");
+                        for (int i = 0; i < jsonArray.length(); i++) {
+                            JSONObject jsonObject = jsonArray.getJSONObject(i);
+                            String tag = jsonObject.optString("Tag");
+                            String vehicle = jsonObject.optString("Vehicle_Id");
+                            tagList.add(tag);
+                            vehicleid.add(vehicle);
+                            tagidAdapter = new ArrayAdapter<>(Alert.this, android.R.layout.simple_spinner_item, tagList);
+                            tagidAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                            tagSpinner.setAdapter(tagidAdapter);
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }, Throwable::getStackTrace);
+
+            }
+        });
+    }
+
 }
--- app/src/main/java/com/example/assettracker/MainActivity2.java
+++ app/src/main/java/com/example/assettracker/MainActivity2.java
@@ -1,15 +1,15 @@
 package com.example.assettracker;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 public class MainActivity2 extends AppCompatActivity {
 
-    Button buttonRegister, buttonCreateAlert;
+    Button buttonRegister, buttonCreateAlert, buttonViewMap, buttonDisableAlert;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +19,8 @@ public class MainActivity2 extends AppCompatActivity {
         /*On click listener for the Cancel button in case the user wants to cancel registration*/
         buttonRegister = findViewById(R.id.buttonasset);
         buttonCreateAlert = findViewById(R.id.buttoncreatealert);
+        buttonViewMap = findViewById(R.id.buttonViewMap);
+        buttonDisableAlert = findViewById(R.id.buttonDisableAlert);
 
         buttonRegister.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -33,6 +35,24 @@ public class MainActivity2 extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Alert.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonViewMap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MapsActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+        buttonDisableAlert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity2.class);
                 startActivity(intent);
                 finish();
             }
